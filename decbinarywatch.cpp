b4623aea0ad372736ace01a935ed65f0b880bbce
Arhangel64 <arhangel64@bk.ru>
2015-07-01 00:28:24 +0300

    first attempt to keep time in binary format

--- decbinarywatch.cpp
+++ decbinarywatch.cpp
@@ -1,27 +1,63 @@
 #include "decbinarywatch.h"
 #include <QSGSimpleRectNode>
+#include <QtMath>
+#include <QtDebug>
 
 DecBinaryWatch::DecBinaryWatch(QQuickItem *parent):
     QQuickItem(parent),
-    m_full(250, 250, 250, 200),
-    m_empty(20, 20, 20, 200),
-    m_time(QTime::currentTime())
+    on_color(250, 250, 250, 200),
+    off_color(20, 20, 20, 200),
+    invalidColor(true),
+    map(0xffffff)
 {
     setFlag(QQuickItem::ItemHasContents);
+    m_time.setTime(QTime::currentTime());
 }
 
 QTime DecBinaryWatch::time() const
 {
-    return m_time;
+    return m_time.getTime();
 }
 
 void DecBinaryWatch::setTime(const QTime& time)
 {
-    if (time != m_time)
+
+    quint32 map = m_time.setTime(time);
+    qDebug() << map;
+    if (map)
     {
-        m_time = time;
         update();
         emit timeChanged();
+    }
+}
+
+QColor DecBinaryWatch::onColor() const
+{
+    return on_color;
+}
+
+void DecBinaryWatch::setOnColor(const QColor& color)
+{
+    if (color != on_color)
+    {
+        on_color = color;
+        invalidColor = true;
+        update();
+    }
+}
+
+QColor DecBinaryWatch::offColor() const
+{
+    return off_color;
+}
+
+void DecBinaryWatch::setOffColor(const QColor& color)
+{
+    if (color != off_color)
+    {
+        off_color = color;
+        invalidColor = true;
+        update();
     }
 }
 
@@ -32,14 +68,61 @@ QSGNode* DecBinaryWatch::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *
     {
         node = new QSGSimpleRectNode();
     }
-    if (time().second()%2 == 0)
+    if (m_time.getSum() & 1)
     {
-        node->setColor(m_empty);
+        node->setColor(off_color);
     }
     else
     {
-        node->setColor(m_full);
+        node->setColor(on_color);
     }
     node->setRect(boundingRect());
+    invalidColor = false;
     return node;
+}
+
+WatchModel::WatchModel():
+    hh(0),
+    hl(0),
+    mh(0),
+    ml(0),
+    sh(0),
+    sl(0)
+{
+}
+
+quint32 WatchModel::setTime(const QTime& time)
+{
+    quint8 lhh(qFloor(time.hour() / 10));
+    quint8 lhl(time.hour() % 10);
+    quint8 lmh(qFloor(time.minute() / 10));
+    quint8 lml(time.minute() % 10);
+    quint8 lsh(qFloor(time.second() / 10));
+    quint8 lsl(time.second() % 10);
+
+    quint32 lsum = lsl | (lsh << 4) | (lml << 8) | (lmh << 12) | (lhl << 16) | (lhh << 24);
+
+    quint32 esum = ~(~(lsum | sum) | (lsum & sum));
+    if (esum)
+    {
+        hh = lhh;
+        hl = lhl;
+        mh = lmh;
+        ml = lml;
+        sh = lsh;
+        sl = lsl;
+
+        sum = lsum;
+    }
+    return esum;
+}
+
+QTime WatchModel::getTime() const
+{
+    return QTime(hh*10+hl, mh*10+ml, sh*10+sl);
+}
+
+quint32 WatchModel::getSum() const
+{
+    return sum;
 }
--- decbinarywatch.h
+++ decbinarywatch.h
@@ -7,10 +7,32 @@
 #include <QTime>
 #include <QDateTime>
 
+class WatchModel
+{
+public:
+    WatchModel();
+
+    quint32 setTime(const QTime&);
+    QTime getTime() const;
+
+    quint32 getSum() const;
+
+private:
+    quint32 sum;
+    quint8 hh;
+    quint8 hl;
+    quint8 mh;
+    quint8 ml;
+    quint8 sh;
+    quint8 sl;
+};
+
 class DecBinaryWatch : public QQuickItem
 {
     Q_OBJECT
     Q_PROPERTY(QTime time READ time WRITE setTime NOTIFY timeChanged)
+    Q_PROPERTY(QColor onColor READ onColor WRITE setOnColor)
+    Q_PROPERTY(QColor offColor READ offColor WRITE setOffColor)
 
 public:
     DecBinaryWatch(QQuickItem *parent = 0);
@@ -18,16 +40,26 @@ public:
     QTime time() const;
     void setTime(const QTime& time);
 
+    QColor onColor() const;
+    void setOnColor(const QColor& color);
+
+    QColor offColor() const;
+    void setOffColor(const QColor& color);
+
     QSGNode* updatePaintNode(QSGNode *, UpdatePaintNodeData *);
 
 signals:
     void timeChanged();
 
 private:
-    QTime m_time;
+    WatchModel m_time;
 
-    QColor m_full;
-    QColor m_empty;
+    QColor on_color;
+    QColor off_color;
+
+    bool invalidColor;
+
+    quint32 map;
 };
 
 #endif // DECBINARYWATCH_H
